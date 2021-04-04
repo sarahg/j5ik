618ffdb7d6f946263c905f1d3d94e8cb51a62bd8
Sarah German <sgerman@pantheon.io>
2021-04-04 16:18:43 -0500

    Add comments

--- led.js
+++ led.js
@@ -1,11 +1,15 @@
+// Include dependencies.
 var Tessel = require("tessel-io");
 var five = require("johnny-five");
 
+// Instantiate a new Board object.
 var board = new five.Board({
   io: new Tessel(),
 });
 
+// Listen for the "ready" event.
 board.on("ready", () => {
+  // Pulse the LED on Port A, Pin 5.
   var led = new five.Led("a5");
   led.pulse(500);
 });
