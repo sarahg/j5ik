9138ddc5c55411b975d463b416fca7a7f959e309
Sarah German <sgerman@pantheon.io>
2021-04-04 16:56:05 -0500

    Visualize input with barcli.

--- sensor-basic.js
+++ sensor-basic.js
@@ -1,11 +1,27 @@
+var Barcli = require("barcli");
 var five = require("johnny-five");
 var Tessel = require("tessel-io");
+
+// Instantiate a board object without the REPL or debug output.
 var board = new five.Board({
   io: new Tessel(),
+  repl: false,
+  debug: false,
 });
 
-board.on("ready", () => {
-  var sensor = new five.Sensor("a7");
+board.on("ready", function() {
+  // Visualize input using Barcli.
+  var range = [0, 100];
+  var graph = new Barcli({
+    label: "My Data",
+    range: range,
+  });
+  var sensor = new five.Sensor({
+    pin: "a7",
+    threshold: 5 // When do we detect a "change" event?
+  });
 
-  sensor.on("change", () => console.log(sensor.value));
+  sensor.on("change", () => {
+    graph.update(sensor.scaleTo(range));
+  });
 });
